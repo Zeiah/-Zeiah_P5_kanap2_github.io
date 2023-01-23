83a08920543e0b0200d51c8a5efff9fc9f571dc6
Margot Salsmann <margot.salsmann@gmail.com>
2023-01-23 14:53:39 +0100

    correction pb image  page cart affichage panier

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -39,14 +39,14 @@ fetch("http://localhost:3000/api/products")
       // ajout div "cart__item_img"
       let cartItemImg = document.createElement("div");
       article.appendChild(cartItemImg);
-      cartItemImg.className = "cart__item_img";
+      cartItemImg.className = "cart__item__img";
 
       let imageCartItemImg = document.createElement("img");
-      imageCartItemImg.appendChild(cartItemImg);
+      cartItemImg.appendChild(imageCartItemImg);
       imageCartItemImg.src = item.imageUrl;
 
       let altCartItemImg = document.createElement("alt");
-      altCartItemImg.appendChild(cartItemImg);
+      cartItemImg.appendChild(altCartItemImg);
       altCartItemImg.alt = item.altTxt;
 
       // ajout div "cart__item__content"
@@ -110,64 +110,7 @@ fetch("http://localhost:3000/api/products")
 });
 
 
-            // cibler cart item
-            // fonction initialiser le panier
-/* for (let i=0; i<itemsPanier.length; i++) {
-
-    let article = document.createElement("article");
-    document.querySelector('cart__item');
-    //data-id="{product-ID}" data-color="{product-color}
-
-    let image = document.createElement("img");
-    article.appendChild(image);
-    image.className = "cart__item_img";
-    image.src = data[i].imageUrl;
-
-    let content = document.createElement('div');
-    article.appendChild(content);
-    content.className = "cart__item__content";
-
-    let description = document.createElement('div');
-    content.appendChild(description);
-    description.className = "cart__item__content__description";
-
-    let titre = document.createElement("h2");
-    description.appendChild(titre);
-    titre.textContent = data[i].name;
-
-    let couleur = document.createElement("p");
-    description.appendChild(couleur);
-    couleur.textContent = data[i].colors;
-
-    let prix = document.createElement("p");
-    description.appendChild(prix);
-    prix.textContent = data[i].price;
-
-    let settings = document.createElement('div');
-    content.appendChild(settings);
-    settings.className = "cart__item__content__settings";
-
-    let quantite = document.createElement('p');
-    settings.appendChild(quantite);
-    quantite.className = "cart__item__content__settings__quantity";
-    quantite.innerText = "Qté:" + quantite;
-    //template string
-    quantite.textContent = data[i].quantite;
-    //input ?
-
-    let supprimer = document.createElement('div');
-    settings.appendChild(supprimer);
-    supprimer.className = "cart__item__content__settings__delete";
-
-    let supprimerItem = document.createElement('p');
-    supprimer.appendChild(supprimerItem);
-    supprimerItem.className = "delete__item";
-    //affichage Supprimer
-    supprimer.innerText = "Supprimer";
-    //mettre un événement localStorage.removeItem ()
-    };
-
-//fonction supprimer item
+/*//fonction supprimer item
 supprimertItem.addEventListener("click", function() {
   localStorage.removeItem(ItemPanier);
   console.log("item supprimé");
@@ -178,7 +121,7 @@ supprimertItem.addEventListener("click", function() {
 document.querySelector('cart__item') = "";
 GetPanier(monPanierModifie);
 
-// parser panier, mapper, fetcher avec id >> map(function (monPanier)){
+// parser panier, mapper, fetcher avec id >> map(function (monPanier)){ */
 
 
   
